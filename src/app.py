d6649e1c966c76c8f0e020a262e5cb76f42fbc36
carlosjaviersepulveda <cj.sepulveda.olivares@gmail.com>
2024-07-21 17:27:07 -0400

    cambios codigo

--- src/app.py
+++ src/app.py
@@ -2,200 +2,79 @@ import pathlib
 from dash import Dash, dcc, html, Input, Output
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
-df01 = pd.read_excel(DATA_PATH.joinpath('data_level_grade_02.xlsx'), sheet_name='DATA')
+df01 = pd.read_csv(DATA_PATH.joinpath('psu_pdt_paes.csv'), sep=";", index_col=0)
 
-grades = df01["NIVEL"].sort_values().unique()
-subjetsALL = df01["ASIGNATURA"].unique()
+df01['LENGUAJE'] = df01['LENGUAJE'].replace(',','.',regex=True)
+df01['LENGUAJE'] = pd.to_numeric(df01['LENGUAJE'])
 
-# Filtro para asiganturas segun nivel elegido
-mask01 = df01[df01["NIVEL"]=="1MEDIO"]
-mask02 = df01[df01["NIVEL"]=="2MEDIO"]
-mask03 = df01[df01["NIVEL"]=="3MEDIO"]
-mask04 = df01[df01["NIVEL"]=="4MEDIO"]
+df01['MATEMÁTICA'] = df01['MATEMÁTICA'].replace(',','.',regex=True)
+df01['MATEMÁTICA'] = pd.to_numeric(df01['MATEMÁTICA'])
 
-# Filtro para áreas
-mask05 = df01[df01['AREA']=='PLAN COMÚN']
-mask06 = df01[df01['AREA']=='CARRERAS']
-mask07 = df01[df01['AREA']=='PROFUNDIZACIÓN HC']
+df01['PROM. LEN-MAT'] = df01['PROM. LEN-MAT'].replace(',','.',regex=True)
+df01['PROM. LEN-MAT'] = pd.to_numeric(df01['PROM. LEN-MAT'])
 
+df01['HISTORIA'] = df01['HISTORIA'].replace(',','.',regex=True)
+df01['HISTORIA'] = pd.to_numeric(df01['HISTORIA'])
 
-# Listas de asignaturas según nivel
-subjets1M = mask01["ASIGNATURA"].unique()
-subjets2M = mask02["ASIGNATURA"].unique()
-subjets3M = mask03["ASIGNATURA"].unique()
-subjets4M = mask04["ASIGNATURA"].unique()
+df01['CIENCIAS'] = df01['CIENCIAS'].replace(',','.',regex=True)
+df01['CIENCIAS'] = pd.to_numeric(df01['CIENCIAS'])
 
-# Listas de asignaturas según área
-subjets_plancomun = mask05["ASIGNATURA"].unique()
-subjets_carreras = mask06['TIPO'].unique()
-subjets_profundizacion = mask07['TIPO'].unique()
+#print(df01.info())
+#print(df01[['LENGUAJE','MATEMÁTICA','PROM. LEN-MAT','HISTORIA', 'CIENCIAS']].head())
 
-# Diccionario con listas de asignaturas plan comun segun nivel
-all_options = {
-    '1MEDIO': subjets1M,
-    '2MEDIO': subjets2M,
-    '3MEDIO': subjets3M,
-    '4MEDIO': subjets4M
-}
-
-# Diccionario con listas de asignaturas por área para 3MEDIO y 4MEDIO
-options_area = {
-    'PLAN COMÚN':subjets_plancomun,
-    'CARRERAS':subjets_carreras,
-    'PROFUNDIZACIÓN HC': subjets_profundizacion
-}
+#df03= df01[df01.columns[0]]
+#print(df03.head())
 
 # Inicio aplicacion Dash
 app = Dash(__name__)
 server=app.server
 
-# Diagrama de la aplicación (Título, Tres listas despegables y un gráfico)
+# Diagrama de la aplicación
 app.layout = html.Div(
-    children=[
+    [
+     html.Div(  children=[
+         
+        html.Div(children='Seleccione una o varias pruebas:', className="menu-title"),
+        dcc.Checklist(
+             id ='opt_test',
+             options = ['LENGUAJE', 'MATEMÁTICA', 'PROM. LEN-MAT','HISTORIA','CIENCIAS'],
+             inline=True,
+             value = ['LENGUAJE'],
+             className='checklist',
+             inputStyle={"margin-left": "20px"}
 
-
-# Marco para tres listas despegables NIVEL, AREA, ASIGNATURA
-html.Div(children=[
-# Lista despegable de NIVELES, segun nivel
-html.Div(
-    children=[
-        html.Div(children='NIVELes', className='menu-title'),
-        dcc.Dropdown(
-            id='level', 
-            options=[ 
-                {'label': nivel, 'value': nivel}
-                
-                for nivel in all_options.keys()
-                
-            ],
-            value='1MEDIO',
-            clearable=False,
-            className='dropdown'
         ),
-    ]),
+     ],
+    className='menu'),
 
-# Lista depegable para Área (Plan Común, Carreras, Profundización HC)
-html.Div(
-    children=[
-        html.Div(children='ÁREAS', className='menu-title'),
-        dcc.Dropdown(
-            id='area', 
-            options=[ 
-               {'label': area, 'value': area}
-                
-                for area in options_area.keys()
-                
-            ],
-            value= 'PLAN COMÚN',
-            clearable=False,
-            className='dropdown',
-           
-        ),
-    ]),
+        dcc.Graph(id='graph_test', config={"displayModeBar": False},  className="wrapper"),
+    ]
+)
 
-# Lista despegable de Asignaturas, segun nivel
-html.Div(
-    children=[
-        html.Div(children="ASIGNATURA", className="menu-title"),
-        dcc.Dropdown(
-            id='subject',
-            clearable=False,
-            className='dropdown',
-                ),
-    
-]),
-],
-className="menu",
-),
-
-# Marco para el gráfico (dcc.Graph está incorporado en la función update_charts)
-    html.Div(id='grafico' , className="wrapper"),
-
-    ])
-
-# callback para cambiar lista despegable de asignaturas de 1MEDIO o 2MEDIO MEDIO segun nivel 
-# o activar lista depegable "AREA" al elegir 3MEDIO o 4MEDIO
 @app.callback(
-    Output('subject', 'options'),
-    Output('subject', 'value'),
-    Output('area','disabled'),
-    Output('area','value'),
-    [Input('level', 'value'),
-     Input('area','value')]
-        
-    )
+    Output('graph_test', 'figure'), 
+    Input('opt_test', 'value'))
 
-# función para cambiar opciones de lista despegable asignaturas segun el nivel y valor inicial
-def set_subject_options(selected_level, selected_area):
-
-    if (selected_level=='2MEDIO' or selected_level=='1MEDIO') and (
-        selected_area=='CARRERAS' or selected_area=='PROFUNDIZACIÓN HC'):
-
-        value_area_ini ='PLAN COMÚN'
+def display_time_series(select_area):
     
-    else:
+    my_string = ", ".join(str(element) for element in select_area)
 
-        value_area_ini = selected_area
-
-    if selected_level == '1MEDIO' or selected_level == '2MEDIO': 
-        
-        options_disabled_area = True
-        
-        options_subjects = [{'label': i, 'value': i} 
-                    for i in all_options[selected_level]]
+    fig_test = px.line(df01, x='AÑO', y=select_area, color='AREA', markers=True,
+                  title= f'RENDIMIENTO ESTUDIANTES en {my_string}',
+                  width=1000, height=380,
+                  labels={'value':'','variable':'PRUEBA','AÑO':'Año'},
+                  color_discrete_map={'HC':'blue','TP-COM':'green','TP-IND':'orange'},
+                  template="simple_white",
+                  )
     
-        value_subject_ini = all_options[selected_level][0]
+    fig_test.update_yaxes(tickfont_weight='bold',title_font_weight='bold',tickfont_size=15)
+    fig_test.update_xaxes(tickfont_weight='bold',title_font_weight='bold')
 
-                  
-        
-    elif selected_level == '3MEDIO' or selected_level == '4MEDIO' :
-        
-        options_disabled_area = False
-
-        options_subjects = [{'label': i, 'value': i} 
-                    for i in options_area[selected_area]]
-    
-        value_subject_ini = options_area[selected_area][0]
-       
-        
-
-    return options_subjects, value_subject_ini, options_disabled_area, value_area_ini
-
-# callback para filtrar gráfico segun nivel y asignatura
-@app.callback(
-        Output('grafico', 'children'),
-        [Input('level', 'value'),
-        Input('subject','value'),
-        Input('area','value')]
-        )
-
-# función para trazar grafico segun nivel, área y asignatura
-def update_charts(nivel,asignatura,area_id):
-
-    if  area_id == 'PLAN COMÚN':
-        select_nivel_subject = df01.query("NIVEL == @nivel and ASIGNATURA == @asignatura")
-        graph_x_axes = 'CURSO'
-    
-    elif area_id == 'CARRERAS' or area_id == 'PROFUNDIZACIÓN HC': 
-        select_nivel_subject = df01.query("AREA == @area_id and TIPO == @asignatura")
-        graph_x_axes = 'ASIGNATURA'
-
-    trace01 = px.bar(select_nivel_subject, x=graph_x_axes, y=['MB','B','S','I','P'],
-                     title= f'RENDIMIENTO ESTUDIANTES en {asignatura}',
-                     width=1000, height=380,
-                     labels={'value':'','variable':'Categorías','CURSO':'Cursos'},
-                     barmode='group',
-                     color_discrete_map={'MB':'blue','B':'green','S':'orange','I':'tomato','P':'darkred'},
-                     template="simple_white",
-                     range_y=[0,1],
-                     )
-    
-    trace01.update_yaxes(tickformat=".1%", tickfont_weight='bold',title_font_weight='bold',tickfont_size=15)
-    trace01.update_xaxes(tickfont_weight='bold', title_font_weight='bold')
-    trace01.update_layout(
+    fig_test.update_layout(
                          hoverlabel_font_color='white',
                          hoverlabel_font_family='Consolas',
                          uniformtext_minsize=5,
@@ -205,10 +84,11 @@ def update_charts(nivel,asignatura,area_id):
                          title_font_size=20,
                          title_x=0.5
                          )
-    new_trace01 = [dcc.Graph(figure=trace01, config={"displayModeBar": False}, className="card")]
-   
-    return new_trace01
 
+
+                     
+    return fig_test
 # cargar en servidor
 if __name__ == '__main__':
     app.run_server(debug=True)
+    
